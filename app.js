2550e9af397c5957559eb9af2b3fef91e92cc884
tauma <contact@part.gg>
2023-11-21 21:54:25 +0100

    add query param + password

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express();
 const port = 3000;
 
 // Function to fetch the top 45 story IDs
-async function fetchTopStoryIds() {
+async function fetchTopStoryIds(totalSize) {
     const response = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json');
-    return response.data.slice(0, 80);
+    return response.data.slice(0, totalSize);
 }
 
 // Function to fetch a story by ID
@@ -18,15 +18,27 @@ async function fetchStory(id) {
 
 // Route to get top stories
 app.get('/topstories', async (req, res) => {
+
+    const password = req.query.password;
+    if (password !== 'totobot') {
+        res.status(401).json({ error: 'Unauthorized' });
+        return;
+    }
+
     try {
-        const ids = await fetchTopStoryIds();
+
+        const totalSize = req.query.size || 80;
+
+        const ids = await fetchTopStoryIds(totalSize);
         const stories = [];
 
-        for (let i = 0; i < ids.length; i += 20) {
-            const batch = ids.slice(i, i + 20);
+        const batchCount = req.query.batchCount || 20;
+
+        for (let i = 0; i < ids.length; i += batchCount) {
+            const batch = ids.slice(i, i + batchCount);
             const storyPromises = batch.map(id => fetchStory(id));
             stories.push(...(await Promise.all(storyPromises)));
-            if (i + 20 < ids.length) await new Promise(resolve => setTimeout(resolve, 1000)); // 1s pause
+            if (i + batchCount < ids.length) await new Promise(resolve => setTimeout(resolve, 1000)); // 1s pause
         }
 
         res.json(stories);
